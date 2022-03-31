a94449638fa78fb3f6036451913b3b81fffd9105
BiG_FoXy <big.foxy.me@gmail.com>
2022-03-31 18:16:44 +0300

    удалил сергея

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const to_pages = ["Index", "Sergey"];
+const to_pages = ["Index"];
 
 const generateHtmlPlugin = (title) => {
     return new HtmlWebpackPlugin({
