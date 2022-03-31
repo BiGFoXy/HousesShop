a303f5d52324c0ed4876e4ec3a76c41202ee265f
Askobarus <salnikov.salnikov2010@yandex.ru>
2022-03-31 17:15:35 +0300

    Revert "Теперь точно удалена"
    This reverts commit 2a0810f2cd86242ff597ce2d6e413bc7cea17c17.

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,1 +1,8 @@
 import './index.scss'
+import './assets/logo.svg'
+import './assets/phoneIcon.svg'
+import './assets/clock.svg'
+import './assets/envelope.svg'
+import './assets/vk.svg'
+import './assets/yt.svg'
+import './assets/wa.svg'
